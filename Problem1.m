d0e1722c7c92e6f7e3bc6d6b704f8cd015802637
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2025-01-21 17:52:20 +0200

    Add files via upload

---
+++ Problem1.m
@@ -0,0 +1,11 @@
+mid = input("midtem grade: ");
+class = input("classwork grade: ");
+final = 50;
+minimumGrade = 0;
+
+if(((mid+class+final)/100) >= 0.6)
+    minimumGrade = 60 - (mid+class)
+else
+    disp("Drop the Class")
+
+end
